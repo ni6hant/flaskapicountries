676cce4b938283b802cbcf8031cd77f63491e0fb
Nishant <ni6hant@gmail.com>
2023-05-22 20:17:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,57 @@
-import requests
-from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from fastapi import FastAPI
+from sqlalchemy import create_engine, Column, Integer, String, BigInteger, DateTime, ForeignKey
+from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
+import requests
+from sqlalchemy.orm import Session, Depends
 
+# Create the FastAPI instance
 app = FastAPI()
 
-# Database Configuration
-db_url = 'postgresql://flaskbloguser:WKNXpBOtYpcvtWBOpjMPFOAe1IgGuWWm@dpg-chgr2367avjbbjpntevg-a.oregon-postgres.render.com/flaskblogdb'
-engine = create_engine(db_url)
-Session = sessionmaker(bind=engine)
+# Configure the database connection
+DATABASE_URL = 'postgresql://flaskbloguser:WKNXpBOtYpcvtWBOpjMPFOAe1IgGuWWm@dpg-chgr2367avjbbjpntevg-a.oregon-postgres.render.com/flaskblogdb'
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
 
 # Country Model
 class Country(Base):
     __tablename__ = 'country'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    cca3 = db.Column(db.String)
-    currency_code = db.Column(db.String)
-    currency = db.Column(db.String)
-    capital = db.Column(db.String)
-    region = db.Column(db.String)
-    subregion = db.Column(db.String)
-    area = db.Column(db.BigInteger)
-    map_url = db.Column(db.String)
-    population = db.Column(db.BigInteger)
-    flag_url = db.Column(db.String)
-    created_at = db.Column(db.DateTime)
-    updated_at = db.Column(db.DateTime)
-    neighbours = db.relationship('CountryNeighbours', backref='country', foreign_keys='CountryNeighbours.country_id')
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    cca3 = Column(String)
+    currency_code = Column(String)
+    currency = Column(String)
+    capital = Column(String)
+    region = Column(String)
+    subregion = Column(String)
+    area = Column(BigInteger)
+    map_url = Column(String)
+    population = Column(BigInteger)
+    flag_url = Column(String)
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
+    neighbours = relationship('CountryNeighbours', backref='country', foreign_keys='CountryNeighbours.country_id')
 
 class CountryNeighbours(Base):
     __tablename__ = 'country_neighbours'
-    id = db.Column(db.Integer, primary_key=True)
-    country_id = db.Column(db.Integer, db.ForeignKey('country.id'))
-    neighbour_country_id = db.Column(db.Integer, db.ForeignKey('country.id'))
-    created_at = db.Column(db.DateTime)
-    updated_at = db.Column(db.DateTime)
+    id = Column(Integer, primary_key=True)
+    country_id = Column(Integer, ForeignKey('country.id'))
+    neighbour_country_id = Column(Integer, ForeignKey('country.id'))
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
 
-Base.metadata.create_all(engine)
+# Create all tables in the database
+Base.metadata.create_all(bind=engine)
+
+# Dependency to get a database session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 # API endpoint to populate countries
 @app.post('/populate_countries')
@@ -49,78 +59,76 @@ def populate_countries():
     response = requests.get('https://restcountries.com/v3.1/all')
     data = response.json()
 
-    session = Session()
+    with SessionLocal() as db:
+        for item in data:
+            # Extract data from the item
+            name = item['name']['common']
+            cca3 = item['cca3']
+            currencies = item.get('currencies', {})
+            currency_data = next(iter(currencies.values()), {})
+            currency_code = list(currencies)[0] if currencies else None
+            currency = currency_data.get('name')
+            capital = item.get('capital', [''])[0]
+            region = item.get('region')
+            subregion = item.get('subregion')
+            area = item['area']
+            map_url = item.get('maps', {}).get('googleMaps')
+            population = item.get('population')
+            flag_url = item['flags']['png']
+            created_at = datetime.now()
+            updated_at = datetime.now()
 
-    for item in data:
-        # Extract data from the item
-        name = item['name']['common']
-        cca3 = item['cca3']
-        currencies = item.get('currencies', {})
-        currency_data = next(iter(currencies.values()), {})
-        currency_code = list(currencies)[0] if currencies else None
-        currency = currency_data.get('name')
-        capital = item.get('capital', [''])[0]
-        region = item.get('region')
-        subregion = item.get('subregion')
-        area = item['area']
-        map_url = item.get('maps', {}).get('googleMaps')
-        population = item.get('population')
-        flag_url = item['flags']['png']
-        created_at = datetime.now()
-        updated_at = datetime.now()
+            # Create a new Country instance
+            country = Country(
+                name=name,
+                cca3=cca3,
+                currency_code=currency_code,
+                currency=currency,
+                capital=capital,
+                region=region,
+                subregion=subregion,
+                area=area,
+                map_url=map_url,
+                population=population,
+                flag_url=flag_url,
+                created_at=created_at,
+                updated_at=updated_at
+            )
 
-        # Create a new Country instance
-        country = Country(
-            name=name,
-            cca3=cca3,
-            currency_code=currency_code,
-            currency=currency,
-            capital=capital,
-            region=region,
-            subregion=subregion,
-            area=area,
-            map_url=map_url,
-            population=population,
-            flag_url=flag_url,
-            created_at=created_at,
-            updated_at=updated_at
-        )
+            # Add the country to the database session
+            db.add(country)
+            db.flush()  # Flush changes to get the auto-generated ID
 
-        # Add the country to the database session
-        session.add(country)
-        session.flush()  # Flush changes to get the auto-generated ID
+            # Create country neighbors
+            borders = item.get('borders', [])
+            for neighbour_cca in borders:
+                neighbour_country = db.query(Country).filter_by(cca3=neighbour_cca).first()
+                if neighbour_country:
+                    country_neighbour = CountryNeighbours(
+                        country_id=country.id,
+                        neighbour_country_id=neighbour_country.id,
+                        created_at=datetime.now(),
+                        updated_at=datetime.now()
+                    )
+                    db.add(country_neighbour)
 
-        # Create country neighbors
-        borders = item.get('borders', [])
-        for neighbour_cca in borders:
-            neighbour_country = session.query(Country).filter_by(cca3=neighbour_cca).first()
-            if neighbour_country:
-                country_neighbour = CountryNeighbours(
-                    country_id=country.id,
-                    neighbour_country_id=neighbour_country.id,
-                    created_at=datetime.now(),
-                    updated_at=datetime.now()
-                )
-                session.add(country_neighbour)
-
-    # Commit the changes to the database
-    session.commit()
+        # Commit the changes to the database
+        db.commit()
 
     return 'Countries populated successfully!'
 
 # API to return all the countries
 @app.get('/country')
-def get_all_countries(request: Request):
-    sort_by = request.query_params.get('sort_by', 'a_to_z')
-    page = int(request.query_params.get('page', 1))
-    limit = int(request.query_params.get('limit', 10))
-    name = request.query_params.get('name')
-    region = request.query_params.get('region')
-    subregion = request.query_params.get('subregion')
-
-    session = Session()
-
-    query = session.query(Country)
+def get_all_countries(
+    sort_by: str = 'a_to_z',
+    page: int = 1,
+    limit: int = 10,
+    name: str = None,
+    region: str = None,
+    subregion: str = None,
+    db: Session = Depends(get_db)
+):
+    query = db.query(Country)
 
     if name:
         query = query.filter(Country.name.ilike(f'%{name}%'))
@@ -176,18 +184,14 @@ def get_all_countries(request: Request):
 
 # API to get a country detail
 @app.get('/country/{country_id}')
-def get_country_detail(country_id: int):
-    session = Session()
-    country = session.query(Country).get(country_id)
+def get_country_detail(country_id: int, db: Session = Depends(get_db)):
+    country = db.query(Country).get(country_id)
 
     if country is None:
-        return JSONResponse(
-            status_code=404,
-            content={
-                'message': 'Country not found',
-                'data': {}
-            }
-        )
+        return {
+            'message': 'Country not found',
+            'data': {}
+        }
 
     response = {
         'message': 'Country detail',
@@ -204,69 +208,27 @@ def get_country_detail(country_id: int):
                 'area': country.area,
                 'map_url': country.map_url,
                 'population': country.population,
-                'flag_url': country.flag_url
-            }
+                'flag_url': country.flag_url,
+            },
+            'neighbours': []
         }
     }
 
-    return response
-
-# API to get country neighbors
-@app.get('/country/{country_id}/neighbour')
-def get_country_neighbours(country_id: int):
-    session = Session()
-    country = session.query(Country).get(country_id)
-
-    if country is None:
-        return JSONResponse(
-            status_code=404,
-            content={
-                'message': 'Country not found',
-                'data': {}
-            }
-        )
-
-    neighbours = session.query(Country).join(
-        CountryNeighbours, Country.id == CountryNeighbours.neighbour_country_id
-    ).filter(
-        CountryNeighbours.country_id == country.id
-    ).all()
-
-    country_neighbours = []
-    for neighbour in neighbours:
-        country_neighbours.append({
-            'id': neighbour.id,
-            'name': neighbour.name,
-            'cca3': neighbour.cca3,
-            'currency_code': neighbour.currency_code,
-            'currency': neighbour.currency,
-            'capital': neighbour.capital,
-            'region': neighbour.region,
-            'subregion': neighbour.subregion,
-            'area': neighbour.area,
-            'map_url': neighbour.map_url,
-            'population': neighbour.population,
-            'flag_url': neighbour.flag_url,
+    for neighbour in country.neighbours:
+        neighbour_country = db.query(Country).get(neighbour.neighbour_country_id)
+        response['data']['neighbours'].append({
+            'id': neighbour_country.id,
+            'name': neighbour_country.name,
+            'cca3': neighbour_country.cca3,
+            'currency_code': neighbour_country.currency_code,
+            'currency': neighbour_country.currency,
+            'capital': neighbour_country.capital,
+            'region': neighbour_country.region,
+            'subregion': neighbour_country.subregion,
+            'area': neighbour_country.area,
+            'map_url': neighbour_country.map_url,
+            'population': neighbour_country.population,
+            'flag_url': neighbour_country.flag_url,
         })
 
-    response = {
-        'message': 'Country neighbours',
-        'data': {
-            'countries': country_neighbours
-        }
-    }
-
     return response
-
-# Create the database if it doesn't exist
-Base.metadata.create_all(bind=engine)
-
-# Create a FastAPI app instance
-app = FastAPI()
-
-# Mount the route handlers
-app.include_router(api_router)
-
-# Run the FastAPI application
-if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8000)
