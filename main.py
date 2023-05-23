e1f7a0e027c962375921d49932af514cf9d6713a
Nishant <ni6hant@gmail.com>
2023-05-23 10:10:42 +0530

    Port fixed for ngork

--- main.py
+++ main.py
@@ -243,4 +243,4 @@ async def get_country_neighbours(country_id: int, db: Session = Depends(get_db))
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+    uvicorn.run(app, host='0.0.0.0', port=5000)
